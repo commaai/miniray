473e94d857475553fe01bfc03f6d7b8feb0815c6
Bruce Wayne <harald.the.engineer@gmail.com>
2026-02-05 13:13:41 -0800

    Dockerfile small cleanup

--- setup_isolation.sh
+++ setup_isolation.sh
@@ -3,3 +3,5 @@ for PROC_ID in $(seq -f '%03g' 0 $(($(nproc) - 1))); do
   # return code 9 = user already exists
   useradd --no-create-home --shell /usr/sbin/nologin --home /nonexistent --system proc${PROC_ID} 2> /dev/null || [ $? -eq 9 ]
 done
+
+groupadd -f video && groupadd -f docker
