748605acbbdc1d13c6a5ce34d855b8012547093a
Harald Schäfer <harald.the.engineer@gmail.com>
2026-08-18 11:48:59 -0700

    Update test_logrotate.sh

--- tests/test_logrotate.sh
+++ tests/test_logrotate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-# setup_isolation.sh installs a logrotate config to cap worker log size; validate it parses
+# just confirm it parses
 logrotate -d /etc/logrotate.d/miniray-worker 2>&1 | grep -q "rotating pattern: /var/log/slurm/miniray-worker"
